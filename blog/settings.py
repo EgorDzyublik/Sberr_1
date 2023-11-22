531cf81cf38bccbda5364558c7f7e758bf367f4d
degor997 <degor997@mail.ru>
2023-11-22 20:39:57 +0300

    Last

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ed*c0*coc9w)5)($&oke)=bgsc=h_*&^5q%ycg#2)5hf45_5##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
